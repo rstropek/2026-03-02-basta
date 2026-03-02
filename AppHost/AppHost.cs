2cd8fd0499c6ecf745901c50d1e0700cc02b7f1f
Rainer Stropek <rainer@software-architects.at>
2026-03-02 10:18:47 +0100

    Refactor AppHost to integrate SQLite and OpenAI parameters

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,5 +1,17 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var chatbot = builder.AddProject<Projects.ChatBot>("chatbot");
+var sqlite = builder.AddSqlite(
+    "chatbot-db",
+    builder.Configuration["Database:path"],
+    builder.Configuration["Database:fileName"]);
+var model = builder.AddParameter(
+    "openai-model", 
+    builder.Configuration["Parameters:openai-model"]!);
+var apiKey = builder.AddParameter("openai-api-key", secret: true);
+
+var chatbot = builder.AddProject<Projects.ChatBot>("chatbot")
+    .WithReference(sqlite)
+    .WithEnvironment("OPENAI_MODEL", model)
+    .WithEnvironment("OPENAI_API_KEY", apiKey);
 
 builder.Build().Run();
